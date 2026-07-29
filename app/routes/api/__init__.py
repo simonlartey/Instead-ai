ec4759ac4c1991a699f482f46c0e75f34f67fc9e
simonlartey <slarte27@colby.edu>
2026-07-28 22:40:23 -0400

    feat(deals): add student deals API

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -1,3 +1,7 @@
+from app.routes.api.deals import deals_api_bp
 from app.routes.api.search import search_api_bp
 
-__all__ = ["search_api_bp"]
+__all__ = [
+    "deals_api_bp",
+    "search_api_bp",
+]
